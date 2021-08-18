3733999ad61ff531d5f0e256f1df2ec455e098a2
ANTONIO LEONARDO ANJOS DOS SANTOS <antonio.santos@maxxidata.com>
2021-08-18 00:36:50 -0300

    Simples doc

---
+++ api.ts
@@ -0,0 +1,19 @@
+/**
+ * Function to add two numbers
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}  
+ */
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+/**
+ * Function to subtract two numbers
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function diff(a: number, b: number) {
+  return a - b;
+}
